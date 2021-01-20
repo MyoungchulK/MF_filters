c7446ced1da484a10e1c55e156cffec06d343b26
MyoungchulK <kmc8907@gmail.com>
2021-01-20 06:50:41 -0600

    shell

--- scripts/wipac_run/ARA_job.sh
+++ scripts/wipac_run/ARA_job.sh
@@ -8,9 +8,24 @@ run=$4
 out=$5
 mode=$6
 
+#temp_dir=/scratch/mkim/MF_filters_out/
+#script_dir=/home/mkim/analysis/MF_filters/scripts/
+
 # run the reconstruction script
-ulimit -s 131072; /home/mkim/analysis/MF_filters/scripts/mf_filter.py ${data} ${ped} ${station} ${run} ${out} ${mode}
+source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh
+cd /home/mkim/analysis/MF_filters/scripts/
+#cd ${script_dir}
+
+#ulimit -s 131072; python3 mf_filter.py ${data} ${ped} ${station} ${run} ${temp_dir} ${mode}
+#ulimit -s 131072; python3 /home/mkim/analysis/MF_filters/scripts/mf_filter.py ${data} ${ped} ${station} ${run} ${out} ${mode}
+/home/mkim/analysis/MF_filters/scripts/mf_filter.py ${data} ${ped} ${station} ${run} ${out} ${mode}
 
 if [ $? -ne 0 ] #error handle if something has gone wrong
 then
-	echo ${data}_${ped}_${station}_${run} >> /data/user/mkim/OMF_filter/ARA0${station}/problems_A${station}_R${run}.txt
+	echo python3 /home/mkim/analysis/MF_filters/scripts/mf_filter.py ${data} ${ped} ${station} ${run} ${out} ${mode} >> /data/user/mkim/OMF_filter/problems_A${station}_R${run}.txt
+
+#else
+
+#    mv ${temp_dir}* ${out}
+
+#fi 
