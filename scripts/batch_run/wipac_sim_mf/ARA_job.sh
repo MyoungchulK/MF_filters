c6ca2fcc19bf212a2f0e1e6a7378ff99da55444e
Myoungchul Kim <kmc8907@gmail.com>
2022-06-22 14:36:44 +0900

    sim mf dag

--- scripts/batch_run/wipac_sim_mf/ARA_job.sh
+++ scripts/batch_run/wipac_sim_mf/ARA_job.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # load in variables
-st=$1
-data=$2
 key=rayl
+st=$1
+year=$2
+data=$3
 
 # run the reconstruction script
 export HDF5_USE_FILE_LOCKING='FALSE'
@@ -11,5 +12,5 @@ source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh
 source /home/mkim/analysis/MF_filters/setup.sh
 cd /home/mkim/analysis/MF_filters/scripts/
 
-python3 /home/mkim/analysis/MF_filters/scripts/sim_script_executor.py ${key} ${st} 2015 ${data}
+python3 /home/mkim/analysis/MF_filters/scripts/sim_script_executor.py ${key} ${st} $(year) ${data}
 
--- scripts/sim_MF_batch_run_maker.py
+++ scripts/sim_MF_batch_run_maker.py
@@ -3,11 +3,24 @@ import numpy as np
 from glob import glob
 from tqdm import tqdm
 
-def get_dag_statement(d_count, d, st, d_type):
+def get_path_info(dat_path, mask_key, end_key):
+
+        mask_idx = dat_path.find(mask_key)
+        if mask_idx == -1:
+            print('Cannot scrap the info from path!')
+            sys.exit(1)
+        mask_len = len(mask_key)
+        end_idx = dat_path.find(end_key, mask_idx + mask_len)
+        val = dat_path[mask_idx + mask_len:end_idx]
+        del mask_idx, mask_len, end_idx
+
+        return val
+
+def get_dag_statement(d_count, d, st, year, d_type):
 
     statements = ""
     statements += f'JOB job_ARA_S{st}_{d_type}_{d_count} ARA_job.sub \n'
-    statements += f'VARS job_ARA_S{st}_{d_type}_{d_count} data="{d}" st="{st}" d_type="{d_type}" d_count="{d_count}"\n\n'
+    statements += f'VARS job_ARA_S{st}_{d_type}_{d_count} data="{d}" st="{st}" year="{year}" d_type="{d_type}" d_count="{d_count}"\n\n'
 
     return statements
 
@@ -27,13 +40,34 @@ dag_file_name = f'{dag_path}A{st}_{d_type}.dag'
 statements = ""
 with open(dag_file_name, 'w') as f:
     f.write(statements)
-
+    
     d_count = 0
     for d in tqdm(data_list):
-        statements = get_dag_statement(d_count, d, st, d_type)
+        
+        st = int(get_path_info(d, 'AraOut.A', '_C'))
+        config = int(get_path_info(d, '_C', '_E'))
+        if st == 3 and config > 5:
+            year = 2018
+            print(d)
+        else:   
+            year = 2015
+
+        statements = get_dag_statement(d_count, d, st, year, d_type)
         with open(dag_file_name, 'a') as f:
             f.write(statements)
         d_count += 1        
 
 print('Dag making is done!')
+
+
+
+
+
+
+
+
+
+
+
+
  
