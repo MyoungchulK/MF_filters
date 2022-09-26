d3a9305eca99b40aeeb6796cb691abf1cc90030a
Myoungchul Kim <kmc8907@gmail.com>
2022-09-27 01:19:12 +0900

    mf hist

--- scripts/mf_hist.py
+++ scripts/mf_hist.py
@@ -23,23 +23,45 @@ s_type = ''
 #s_type = 'noise_'
 #s_type = 'signal_F1_'
 
-knwon_issue = known_issue_loader(Station)
-bad_runs = knwon_issue.get_knwon_bad_run(use_qual = True)
-del knwon_issue
+bad_path = f'../data/rayl_runs/rayl_run_A{station}.txt'
+bad_run_arr = []
+with open(bad_path, 'r') as f:
+    for lines in f:
+        run_num = int(lines)
+        bad_run_arr.append(run_num)
+bad_run_arr = np.asarray(bad_run_arr, dtype = int)
+del bad_path
 
 d_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/mf{d_type}/*{s_type}*'
 d_list, d_run_tot, d_run_range = file_sorter(d_path)
+if d_type != '_sim':
+    q_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/qual_cut/'
+    c_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/cw_cut/'
 
 mf_bins = np.linspace(0,15,1500+1)
 mf_bin_center = (mf_bins[1:] + mf_bins[:-1]) / 2
 mf_bin_len = len(mf_bin_center)
 mf_hist = np.full((2, mf_bin_len, config_len), 0, dtype = int)
+if d_type != '_sim':
+    mf_hist_rf = np.copy(mf_hist)
+    mf_hist_cal = np.copy(mf_hist)
+    mf_hist_soft = np.copy(mf_hist)
+    mf_hist_rf_clean = np.copy(mf_hist)
+    mf_hist_cal_clean = np.copy(mf_hist)
+    mf_hist_soft_clean = np.copy(mf_hist)
+    mf_list = []
+    mf_list_rf = []
+    mf_list_cal = []
+    mf_list_soft = []
+    mf_list_rf_clean = []
+    mf_list_cal_clean = []
+    mf_list_soft_clean = [] 
 
 num_evts = np.full((config_len), 0, dtype = float)
 
 for r in tqdm(range(len(d_run_tot))):
 
-    if d_run_tot[r] in bad_runs:
+    if d_run_tot[r] in bad_run_arr:
         print('bad run:', d_list[r], d_run_tot[r])
         continue
 
@@ -47,14 +69,80 @@ for r in tqdm(range(len(d_run_tot))):
     c_idx = ara_run.get_config_number() - 1
     del ara_run
 
+    if d_type != '_sim':
+        q_name = f'{q_path}qual_cut_A{Station}_R{d_run_tot[r]}.h5'
+        c_name = f'{q_path}cw_cut_A{Station}_R{d_run_tot[r]}.h5'
+        hf_q = h5py.File(q_name, 'r')
+        q_cut = hf_q['tot_qual_cut_sum'][:]
+        trig_type = hf_q['trig_type'][:]
+        hf_c = h5py.File(c_name, 'r')
+        q_cut += hf_c['cw_qual_cut_sum'][:]
+        q_cut = q_cut.astype(int)
+        del q_name, c_name, hf_q, hf_c
+        rf_idx = trig_type == 0
+        cal_idx = trig_type == 1
+        soft_idx = trig_type == 1
+        rf_c_idx = np.logical_and(rf_idx, q_cut == 0)
+        cal_c_idx = np.logical_and(cal_idx, q_cut == 0)
+        soft_c_idx = np.logical_and(soft_idx, q_cut == 0)
+        del trig_type, q_cut
+
     hf = h5py.File(d_list[r], 'r')
     evt_wise = hf['evt_wise'][:]
     del hf
+    if d_type != '_sim':
+            evt_w_rf = np.copy(evt_wise)
+            evt_w_rf[:, ~rf_idx] = np.nan
+            evt_w_cal = np.copy(evt_wise)
+            evt_w_cal[:, ~cal_idx] = np.nan
+            evt_w_soft = np.copy(evt_wise)
+            evt_w_soft[:, ~soft_idx] = np.nan
+            evt_w_rf_c = np.copy(evt_wise)
+            evt_w_rf_c[:, ~rf_c_idx] = np.nan
+            evt_w_cal_c = np.copy(evt_wise)
+            evt_w_cal_c[:, ~cal_c_idx] = np.nan
+            evt_w_soft_c = np.copy(evt_wise)
+            evt_w_soft_c[:, ~soft_c_idx] = np.nan
 
-    num_evts[c_idx] += np.count_nonzero(~np.isnan(evt_wise))
+            mf_h_rf = np.full((2, mf_bin_len), 0, dtype = int)
+            mf_h_cal = np.full((2, mf_bin_len), 0, dtype = int)
+            mf_h_soft = np.full((2, mf_bin_len), 0, dtype = int)
+            mf_h_rf_c = np.full((2, mf_bin_len), 0, dtype = int)
+            mf_h_cal_c = np.full((2, mf_bin_len), 0, dtype = int)
+            mf_h_soft_c = np.full((2, mf_bin_len), 0, dtype = int)
+            for a in range(2):
+                mf_h_rf[a] = np.histogram(evt_w_rf[a], bins = mf_bins)[0]
+                mf_h_cal[a] = np.histogram(evt_w_cal[a], bins = mf_bins)[0]
+                mf_h_soft[a] = np.histogram(evt_w_soft[a], bins = mf_bins)[0]
+                mf_h_rf_c[a] = np.histogram(evt_w_rf_c[a], bins = mf_bins)[0]
+                mf_h_cal_c[a] = np.histogram(evt_w_cal_c[a], bins = mf_bins)[0]
+                mf_h_soft_c[a] = np.histogram(evt_w_soft_c[a], bins = mf_bins)[0]
+            mf_hist_rf[:, :, c_idx] += mf_h_rf
+            mf_hist_cal[:, :, c_idx] += mf_h_cal
+            mf_hist_soft[:, :, c_idx] += mf_h_soft
+            mf_hist_rf_clean[:, :, c_idx] += mf_h_rf_c
+            mf_hist_cal_clean[:, :, c_idx] += mf_h_cal_c
+            mf_hist_soft_clean[:, :, c_idx] += mf_h_soft_c
+            mf_list_rf.append(mf_h_rf)
+            mf_list_cal.append(mf_h_cal)
+            mf_list_soft.append(mf_h_soft)
+            mf_list_rf_clean.append(mf_h_rf_c)
+            mf_list_cal_clean.append(mf_h_cal_c)
+            mf_list_soft_clean.append(mf_h_soft_c)
+            del evt_w_rf, evt_w_cal, evt_w_soft, evt_w_rf_c, evt_w_cal_c, evt_w_soft_c
+            del rf_idx, cal_idx, soft_idx, rf_c_idx, cal_c_idx, soft_c_idx
 
-    mf_hist[0, :, c_idx] += np.histogram(evt_wise[0], bins = mf_bins)[0] 
-    mf_hist[1, :, c_idx] += np.histogram(evt_wise[1], bins = mf_bins)[0]
+            num_evts[c_idx] += np.count_nonzero(~np.isnan(evt_w_rf_c[0]))
+    else:
+            num_evts[c_idx] += np.count_nonzero(~np.isnan(evt_wise[0]))
+
+    mf_h = np.full((2, mf_bin_len), 0, dtype = int)
+    for a in range(2):
+        mf_h[a] = np.histogram(evt_wise[a], bins = mf_bins)[0]
+    mf_hist[:, :, c_idx] += mf_h
+    if d_type != '_sim':
+        mf_list.append(mf_h)
+    del evt_w_rf, evt_w_cal, evt_w_soft, evt_w_rf_c, evt_w_cal_c, evt_w_soft_c
     del evt_wise, c_idx
 
 path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/Hist/'
@@ -66,6 +154,20 @@ hf = h5py.File(file_name, 'w')
 hf.create_dataset('mf_bins', data=mf_bins, compression="gzip", compression_opts=9)
 hf.create_dataset('mf_bin_center', data=mf_bin_center, compression="gzip", compression_opts=9)
 hf.create_dataset('mf_hist', data=mf_hist, compression="gzip", compression_opts=9)
+if d_type != '_sim':
+    hf.create_dataset('mf_hist_rf', data=mf_hist_rf, compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_hist_cal', data=mf_hist_cal, compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_hist_soft', data=mf_hist_soft, compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_hist_rf_clean', data=mf_hist_rf_clean, compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_hist_cal_clean', data=mf_hist_cal_clean, compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_hist_soft_clean', data=mf_hist_soft_clean, compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_list', data=np.asarray(mf_list), compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_list_rf', data=np.asarray(mf_list_rf), compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_list_cal', data=np.asarray(mf_list_cal), compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_list_soft', data=np.asarray(mf_list_soft), compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_list_rf_clean', data=np.asarray(mf_list_rf_clean), compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_list_cal_clean', data=np.asarray(mf_list_cal_clean), compression="gzip", compression_opts=9)
+    hf.create_dataset('mf_list_soft_clean', data=np.asarray(mf_list_soft_clean), compression="gzip", compression_opts=9)
 hf.create_dataset('num_evts', data=num_evts, compression="gzip", compression_opts=9)
 hf.close()
 print('file is in:',path+file_name)
