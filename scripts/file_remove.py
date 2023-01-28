09c6ecc41fa6d8714ddf01868983672984d613fa
Myoungchul Kim <kmc8907@gmail.com>
2023-01-28 12:25:06 +0900

    aaaa

--- scripts/file_remove.py
+++ scripts/file_remove.py
@@ -13,12 +13,13 @@ from tools.ara_run_manager import run_info_loader
 from tools.ara_known_issue import known_issue_loader
 
 Station = int(sys.argv[1])
+Type = str(sys.argv[2])
 
 # sort
-d_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/cw_ratio_full/*'
+d_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/{Type}/*'
 d_list, d_run_tot, d_run_range = file_sorter(d_path)
 
-b_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/cw_ratio_full/cw_ratio_full_'
+b_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/{Type}/{Type}_'
 
 for r in tqdm(range(len(d_run_tot))):
     
