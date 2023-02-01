8487d26440afad817e6c87e3348ef472f5ff2b03
MyoungchulK <kmc8907@gmail.com>
2023-02-01 01:34:37 -0600

    qdqfqw

--- scripts/batch_run/wipac/ARA_job.sh
+++ scripts/batch_run/wipac/ARA_job.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # load in variables
-key=dupl
+key=cw_band
 station=$1
 run=$2
+blind=0
 condor_run=0
 not_override=0
+qual_2nd=0
 l2_data=0
+no_tqdm=1
 
 # run the reconstruction script
 export HDF5_USE_FILE_LOCKING='FALSE'
@@ -14,5 +17,5 @@ source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh
 source /home/mkim/analysis/MF_filters/setup.sh
 cd /home/mkim/analysis/MF_filters/scripts/
 
-python3 /home/mkim/analysis/MF_filters/scripts/script_executor.py -k ${key} -s ${station} -r ${run} -c ${condor_run} -n ${not_override} -l ${l2_data}
+python3 /home/mkim/analysis/MF_filters/scripts/script_executor.py -k ${key} -s ${station} -r ${run} -b ${blind} -c ${condor_run} -n ${not_override} -q ${qual_2nd} -t ${no_tqdm} -l ${l2_data}
 
