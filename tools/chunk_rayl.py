3e80400334d125272351d9cb8029a27dde156b86
MyoungchulK <kmc8907@gmail.com>
2023-02-23 06:53:14 -0600

    qwdqwdq

--- tools/chunk_rayl.py
+++ tools/chunk_rayl.py
@@ -56,8 +56,8 @@ def rayl_collector(Data, Ped, analyze_blind_dat = False, use_l2 = False, use_sim
    
     # clean soft trigger 
     clean_rf_idx = np.logical_and(~tot_cuts, trig_type == 0)
-    #clean_soft_idx = np.logical_and(~tot_cuts, trig_type == 2)
-    clean_soft_idx = np.logical_and(~tot_cuts, trig_type == 0)
+    clean_soft_idx = np.logical_and(~tot_cuts, trig_type == 2)
+    #clean_soft_idx = np.logical_and(~tot_cuts, trig_type == 0)
     clean_soft_entry = entry_num[clean_soft_idx]
     num_clean_softs = np.count_nonzero(clean_soft_idx)
     print(f'Number of clean soft event is {num_clean_softs}') 
