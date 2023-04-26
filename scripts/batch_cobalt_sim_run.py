e7f7be4cd9d5b6425199114d3f2aa7dae4a3da34
MyoungchulK <kmc8907@gmail.com>
2023-04-26 00:23:26 -0500

    adsqwfq

--- scripts/batch_cobalt_sim_run.py
+++ scripts/batch_cobalt_sim_run.py
@@ -15,8 +15,8 @@ def cobalt_run_loader(Key = None, Station = None, Act_Evt = None):
     print('event range:', Act_Evt)
     Yrs = 2015
   
-    #d_path = '/data/user/brianclark/for_Uzair/E2_nomag_art/AraOut.setup_nue_cc_art_E2.txt.run*'
-    d_path = f'/misc/disk19/users/mkim/OMF_filter/ARA0{Station}/sim_signal_full/AraOut*root'
+    d_path = os.path.expandvars("$OUTPUT_PATH") + f'/ARA0{Station}/sim_signal_full/AraOut*root'
+    #d_path = f'/misc/disk19/users/mkim/OMF_filter/ARA0{Station}/sim_signal_full/AraOut*root'
     #d_path = f'/misc/disk19/users/mkim/OMF_filter/ARA0{Station}/sim_noise_full/AraOut*root'
     lists = glob(d_path)
     print(len(lists))
