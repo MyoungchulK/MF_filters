2858fbb63fe4aa39fbc1daa8bbea4df7c3598b92
MyoungchulK <kmc8907@gmail.com>
2023-04-26 00:20:06 -0500

    qqqq

--- scripts/batch_run/wipac_sim_signal_result_config/ARA_job.sh
+++ scripts/batch_run/wipac_sim_signal_result_config/ARA_job.sh
@@ -10,14 +10,17 @@ fla=$4
 sim_run=$5
 year=2015
 not_override=0
-user_path=/misc/disk19/users/
+user_path=/misc/disk19/users/mkim/OMF_filter/
 if [ -d "$user_path" ]; then
     echo "There is ${user_path}"
 else
-    echo "Switch to /data/user/"
-    user_path=/data/user/
+    #echo "Switch to /data/user/"
+    #user_path=/data/user/
+    echo "Switch to /data/ana/ARA/"
+    user_path=/data/ana/ARA/
 fi
-data=${user_path}mkim/OMF_filter/ARA0${st}/sim_${sim_key}_full/AraOut.${sim_key}_E${en}_F${fla}_A${st}_R${run}.txt.run${sim_run}.root
+data=${user_path}ARA0${st}/sim_${sim_key}_full/AraOut.${sim_key}_E${en}_F${fla}_A${st}_R${run}.txt.run${sim_run}.root
+
 
 # run the reconstruction script
 export HDF5_USE_FILE_LOCKING='FALSE'
