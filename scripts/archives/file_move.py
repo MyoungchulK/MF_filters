e6d8508f2e0b71c67222d6ae109e3e6485d8d163
Myoungchul Kim <kmc8907@gmail.com>
2023-05-11 18:46:39 +0900

    clean up

--- scripts/archives/file_move.py
+++ scripts/archives/file_move.py
@@ -11,26 +11,27 @@ from tools.ara_run_manager import file_sorter
 from tools.ara_utility import size_checker
 from tools.ara_run_manager import run_info_loader
 from tools.ara_known_issue import known_issue_loader
-from tools.ara_run_manager import get_path_info_v2
 
 Station = int(sys.argv[1])
 Type = str(sys.argv[2])
 
 # sort
-d_path = os.path.expandvars("$OUTPUT_PATH") + f'/ARA0{Station}/{Type}/*'
+r_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/cw_ratio_full/*'
+r_list, r_run_tot, r_run_range, r_len = file_sorter(r_path)
+
+d_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/{Type}/*'
 d_list, d_run_tot, d_run_range,d_len = file_sorter(d_path)
 
-t_path = os.path.expandvars("$OUTPUT_PATH") + f'/ARA0{Station}/{Type}_temp/'
+d_idx = np.in1d(d_run_tot, r_run_tot)
+
+t_path = os.path.expandvars("$OUTPUT_PATH") + f'/OMF_filter/ARA0{Station}/{Type}_temp/'
 if not os.path.exists(t_path):
     os.makedirs(t_path)
 
 for r in tqdm(range(len(d_run_tot))):
    
-    sim_run = int(get_path_info_v2(d_list[r], 'txt.run', '.root'))
-    if sim_run > 99:
-        continue
+    if d_idx[r]: 
+        MV_CMD = f'mv {d_list[r]} {t_path}'
+        call(MV_CMD.split(' '))
 
-    MV_CMD = f'mv {d_list[r]} {t_path}'
-    call(MV_CMD.split(' '))
-    print(MV_CMD)
     
