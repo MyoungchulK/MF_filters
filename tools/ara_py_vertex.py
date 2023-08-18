46c01105eee1ea9d5c21af1250ea9815889ff060
Myoungchul Kim <kmc8907@gmail.com>
2023-08-18 16:26:41 +0900

    reco handler

--- tools/ara_py_vertex.py
+++ tools/ara_py_vertex.py
@@ -1,214 +1,151 @@
 import os
 import numpy as np
-from scipy.signal import fftconvolve
-from scipy.signal import correlation_lags
-from scipy.signal import hilbert
+from scipy.ndimage import uniform_filter1d
 from tqdm import tqdm
 import h5py
 from itertools import combinations
 
 # custom lib
 from tools.ara_constant import ara_const
-from tools.ara_run_manager import get_pair_info
+from tools.ara_known_issue import known_issue_loader
+from tools.ara_data_load import ara_geom_loader
 
 ara_const = ara_const()
 num_ants = ara_const.USEFUL_CHAN_PER_STATION
 num_pols = ara_const.POLARIZATION
+num_pols_com = int(num_pols + 1)
 
-class py_hit_time_loader:
+class py_reco_handler:
 
-    def __init__(self, pad_len, dt, sum_win = 25):
+    def __init__(self, st, run, dt, hit_thres, sum_win = 25, num_ants_cut = 3, use_debug = False):
 
-        self.ant_range = np.arange(num_ants, dtype = int)
-        self.pad_len = pad_len
-        self.pad_range = np.arange(self.pad_len, dtype = int)
         self.dt = dt
         self.sum_win = sum_win
-        self.sum_win_idx = int(np.round(sum_win / dt))
-        self.sum_win_idx_float = float(self.sum_win_idx)
-        self.sum_win_one = np.full((self.sum_win_idx, num_ants), 1, dtype = float)
+        self.sum_win_idx = int(np.round(self.sum_win / self.dt))
+        self.hit_thres = hit_thres
+        self.num_ants_cut = num_ants_cut
+        self.use_debug = use_debug
+
+        ## ch info
+        self.ant_range = np.arange(num_ants, dtype = int)
+        self.polarizations = (self.ant_range > int(num_pols // 2 - 1)).astype(int)
+        known_issue = known_issue_loader(st)
+        self.can_use_ch = known_issue.get_bad_antenna(run, good_ant_true = True) # good chs in bool
+        ara_geom = ara_geom_loader(st, int(2015))
+        self.ant_pos = ara_geom.get_ant_xyz() # (xyz, chs)
+        del known_issue, ara_geom
 
     def get_max_info(self):
 
-        self.max_bin = np.nanargmax(self.wf2, axis = 0)
-        self.max_val = self.wf2[self.max_bin, self.ant_range]
+        ## max bin, volt, and time of al chs
+        self.max_bin = np.nanargmax(self.pad_v, axis = 0)
+        self.max_val = self.pad_v[self.max_bin, self.ant_range]
+        self.max_time = self.pad_t[self.max_bin, self.ant_range]
 
     def get_sqrt_volt_sum_wf(self):
 
-        self.wf2 = np.sqrt(fftconvolve(self.wf2, self.sum_win_one, 'same', axes = 0) / self.sum_win_idx_float)
+        ## rolling mean of all chs by uniform_filter1d()
+        self.pad_v = np.sqrt(uniform_filter1d(self.pad_v, size = self.sum_win_idx, mode = 'constant', axis = 0))
 
     def get_mean_sigma_in_no_max(self):
 
+        ## half signal window
+        bin_8 = self.pad_num // 8
+       
+        ## remove signal windo info 
+        pad_no_max = np.full(self.pad_v.shape, np.nan, dtype = float)
+        for ant in range(num_ants):
+            pad_no_max[:self.pad_num[:, ant], ant] = self.pad_v[:self.pad_num[:, ant], ant]
+            front_idx = int(self.max_bin[ant] - bin_8[ant])
+            if front_idx < 0:
+                front_idx = 0
+            pad_no_max[front_idx:self.max_bin[ant] + bin_8[ant] + 1, ant] = np.nan
+            del front_idx
+        if use_debug:
+            self.pad_no_max = pad_no_max
+            self.bin_8 = bin_8
 
-class py_interferometers:
+        ## mean sigma
+        self.pad_mean = np.nanmean(pad_no_max, axis = 0)
+        self.pad_sigma = np.nanstd(pad_no_max, axis = 0)
+        del bin_8, pad_no_max
+           
+    def get_ch_sliding_v2_snr_uw(self): 
 
-    def __init__(self, pad_len, dt, st, run = None, get_sub_file = False, use_ele_max = False, use_debug = False, verbose = False):
+        ## to power and rolling mean
+        self.get_sqrt_volt_sum_wf()
 
-        self.verbose = verbose
-        self.dt = dt
-        self.st = st
-        self.run = run
-        self.use_debug = use_debug
-        self.use_ele_max = use_ele_max
+        ## max val, time and bin
+        self.get_max_info()
 
-        if get_sub_file:
-            self.get_zero_pad(pad_len)
-            self.lags = correlation_lags(self.double_pad_len, self.double_pad_len, 'same') * self.dt
-            self.lag_len = len(self.lags)
-            self.pairs, self.pair_len, self.v_pairs_len = get_pair_info(self.st, self.run, verbose = self.verbose)
-            self.pair_range = np.arange(self.pair_len, dtype = int)
-            self.get_arrival_time_tables()
-            self.get_coval_time()
-            if self.verbose:
-                print('sub tools are ready!')
-        else:                
-            self.lags = correlation_lags(pad_len, pad_len, 'full') * self.dt    
-            self.lag_len = len(self.lags)
+        ## mean sigma
+        self.get_mean_sigma_in_no_max()
 
-    def get_zero_pad(self, pad_len):
-    
-        self.double_pad_len = pad_len * 2 
-        self.pad_one = np.full((self.double_pad_len, num_ants), 1, dtype = float) 
-        self.zero_pad = np.full((self.double_pad_len, num_ants), 0, dtype = float)
-        self.quater_idx = pad_len // 2
-        if self.verbose:
-            print('pad is on!')
+        ## snr and hit time
+        self.snr_arr = (self.max_val - self.pad_mean) / self.pad_sigma        
+        self.hit_time_arr = self.max_time
+        nega_sigma_idx = self.pad_sigma <= 0
+        if np.count_nonzeeo(nega_sigma_idx) > 0:
+            self.snr_arr[nega_sigma_idx] = 0
+            self.hit_time_arr[nega_sigma_idx] = -9999
+        if use_debug:
+            self.nega_sigma_idx = nega_sigma_idx
+        del nega_sigma_idx
 
-    def get_arrival_time_tables(self):
+    def get_id_hits_prep_to_vertex(self, pad_v, pad_t, pad_num):
 
-        table_path = os.path.expandvars("$OUTPUT_PATH") + f'/ARA0{self.st}/arr_time_table/'
-        table_name = f'arr_time_table_A{self.st}.h5'
-        if self.verbose:
-            print('arrival time table:', table_path + table_name)        
+        ## input waveform
+        self.pad_v = pad_v ** 2
+        self.pad_t = pad_t
+        self.pad_num = pad_num
 
-        table_hf = h5py.File(table_path + table_name, 'r')
-        self.theta = 90 - table_hf['theta_bin'][:] # nadir to elevation angle
-        self.num_thetas = len(self.theta)
-        self.phi = table_hf['phi_bin'][:]
-        self.num_phis = len(self.phi)
-        self.radius = table_hf['radius_bin'][:]
-        if self.verbose:
-            print('radius param:', self.radius)
-        self.num_rads = len(self.radius)
-        self.num_ray_sol = len(table_hf['num_ray_sol'][:])
-        self.num_angs = int(2)
-        self.num_pols = num_pols
-        self.num_pols_com = int(num_pols + 1)
-        arr_table = table_hf['arr_time_table'][:]
-        del table_path, table_name, table_hf
+        ## hit time and snr
+        self.get_ch_sliding_v2_snr_uw()
+        if use_debug == False:
+            del self.pad_v, self.pad_t, self.pad_num, self.max_bin, self.max_val, self.max_time, self.pad_mean, self.pad_sigma
+
+        ## hit check. tag only uesful ch and ch that has bigger than snr
+        is_ch_hit = np.logical_and(self.can_use_ch, self.snr_arr > self.hit_thres)
+        n_hits = np.full((num_pols_com), 0, dtype = int) # v, h, both
+        n_hits[0] = np.nansum(is_ch_hit[self.polarizations == 0])
+        n_hits[1] = np.nansum(is_ch_hit[self.polarizations == 1])
+        n_hits[2] = np.nansum(is_ch_hit)
+        if use_debug == False:
+            del self.snr_arr      
  
-        self.table = arr_table[:, :, :, self.pairs[:, 0], :] - arr_table[:, :, :, self.pairs[:, 1], :]
-        self.table = np.transpose(self.table, (0, 1, 2, 4, 3))
-        self.sky_map_shape = (self.num_pols_com, self.num_thetas * self.num_phis, self.num_rads, self.num_ray_sol)
-        if self.use_debug or self.use_ele_max:
-            self.table_ori_shape = self.table.shape
-            self.table_pol_shape = (self.num_pols_com, self.num_thetas, self.num_phis, self.num_rads, self.num_ray_sol)
-        self.coord_shape = (self.num_pols_com, self.num_angs, self.num_rads, self.num_ray_sol)
-        self.table = np.reshape(self.table, (-1, self.num_rads, self.num_ray_sol, self.pair_len))
-        self.table_shape = self.table.shape
-        if self.verbose:
-            print('arr table shape:', self.table_shape)
+        ## ch selection. tag only uesful ch, ch that has bigger than snr, same pol, and numher of hit that bigger than cut
+        ch_for_reco = np.full((num_ants, num_pols_com), 0, dtype = int)
+        ch_for_reco[:, 0] = np.all((self.polarizations == 0, is_ch_hit, n_hits[0] > num_ants_cut), axis = 0)
+        ch_for_reco[:, 1] = np.all((self.polarizations == 1, is_ch_hit, n_hits[1] > num_ants_cut), axis = 0)
+        ch_for_reco[:, 2] = np.all((is_ch_hit, n_hits[2] > num_ants_cut), axis = 0)
+        if use_debug:
+            self.is_ch_hit = is_ch_hit
+            self.n_hits = n_hits
+            self.ch_for_reco = ch_for_reco
+        del is_ch_hit, n_hits       
 
-        table_p1 = np.reshape(np.transpose(arr_table[:, :, :, self.pairs[:, 0], :], (0, 1, 2, 4, 3)), self.table_shape)
-        table_p2 = np.reshape(np.transpose(arr_table[:, :, :, self.pairs[:, 1], :], (0, 1, 2, 4, 3)), self.table_shape)
-        self.bad_arr = np.logical_or(table_p1 < -100, table_p2 < -100)
-        del table_p1, table_p2, arr_table
-    
-        self.pol_range = np.arange(self.num_pols_com, dtype = int)
-        self.rad_range = np.arange(self.num_rads, dtype = int)
-        self.ray_range = np.arange(self.num_ray_sol, dtype = int)
-        self.ang_range = np.arange(self.num_angs, dtype = int)
-
-    def get_coval_time(self):
-
-        self.p0_idx = np.floor((self.table - self.lags[0]) / self.dt).astype(int)
-        self.p0_idx[self.p0_idx < 0] = 0
-        self.p0_idx[self.p0_idx >= self.lag_len - 1] = self.lag_len - 2
-
-        self.int_factor = (self.table - self.lags[self.p0_idx])/self.dt
-        if self.verbose:
-            print('coval time is on!')
-
-    def get_coval_sample(self):
-
-        coval = np.diff(self.corr, axis = 0)[self.p0_idx, self.pair_range] * self.int_factor + self.corr[self.p0_idx, self.pair_range]
-        coval[self.bad_arr] = 0
-        if self.use_debug:
-            self.coval = np.reshape(coval, self.table_ori_shape)
-
-        sky_map = np.full(self.sky_map_shape, np.nan, dtype = float) # array dim (# of pols, # of thetas X # of phis, # of rs, # of rays) 
-        sky_map[0] = np.nansum(coval[:, :, :, :self.v_pairs_len], axis = 3)
-        sky_map[1] = np.nansum(coval[:, :, :, self.v_pairs_len:], axis = 3)
-        sky_map[2] = np.nansum(sky_map[:2] * self.wei_pol[:, np.newaxis, np.newaxis, np.newaxis], axis = 0)
-        if self.use_ele_max:
-            sky_map_ele = np.reshape(sky_map, self.table_pol_shape) # array dim (# of pols, # of thetas, # of phis, # of rs, # of rays)
-            self.coval_ele_max = np.nanmax(sky_map_ele, axis = 2) # array dim (# of pols, # of thetas, # of rs, # of rays)
-            del sky_map_ele
-        if self.use_debug:
-            self.sky_map = np.reshape(sky_map, self.table_pol_shape)
-        del coval
-
-        coord = np.nanargmax(sky_map, axis = 1)
-        self.coval_max = sky_map[self.pol_range[:, np.newaxis, np.newaxis], coord, self.rad_range[np.newaxis, :, np.newaxis], self.ray_range[np.newaxis, np.newaxis, :]] # array dim (# of pols, # of rs, # of rays)
-        self.coord_max = np.full(self.coord_shape, np.nan, dtype = float) # array dim (# of pols, theta and phi, # of rs, # of rays)
-        self.coord_max[:, 0] = self.theta[coord // self.num_phis]
-        self.coord_max[:, 1] = self.phi[coord % self.num_phis]
-        del sky_map, coord
-
-    def get_padded_wf(self, pad_v):
-
-        self.zero_pad[:] = 0
-        self.zero_pad[self.quater_idx:-self.quater_idx] = pad_v
-
-    def get_cross_correlation(self):
-
-        # fft correlation w/ multiple array at once
-        self.corr = fftconvolve(self.zero_pad[:, self.pairs[:, 0]], self.zero_pad[::-1, self.pairs[:, 1]], 'same', axes = 0)
-        if self.use_debug:
-            self.corr_nonorm = np.copy(self.corr)
-
-        # normalization factor by wf weight
-        nor_fac = fftconvolve(self.zero_pad**2, self.pad_one, 'same', axes = 0)
-        nor_fac = np.sqrt(nor_fac[::-1, self.pairs[:, 0]] * nor_fac[:, self.pairs[:, 1]])
-        self.corr /= nor_fac
-        self.corr[np.isnan(self.corr) | np.isinf(self.corr)] = 0 # convert x/nan result
-        if self.use_debug:
-            self.nor_fac = nor_fac
-        else:
-            del nor_fac
-
-        # hilbert
-        self.corr = np.abs(hilbert(self.corr, axis = 0))
-    
-    def get_sky_map(self, pad_v, weights = None, wei_pol = None):
-
-        #zero pad
-        self.get_padded_wf(pad_v)
-        
-        # correlation
-        self.get_cross_correlation()
-
-        self.wei_pol = np.full((self.num_pols), 1, dtype = float)
-        if weights is not None:
-            self.corr *= weights
-            self.wei_pol = wei_pol
-
-        # coval
-        self.get_coval_sample()
-        if self.use_debug == False:
-            del self.corr
-
-def get_products(weights, pairs, v_pairs_len):
-   
-    wei_pairs = weights[pairs[:, 0]] * weights[pairs[:, 1]]
-    wei_pol = np.full((num_pols, weights.shape[1]), np.nan, dtype = float)
-    wei_pol[0] = np.nansum(wei_pairs[:v_pairs_len], axis = 0)
-    wei_pol[1] = np.nansum(wei_pairs[v_pairs_len:], axis = 0)
-    wei_pairs[:v_pairs_len] /= wei_pol[0][np.newaxis, :]
-    wei_pairs[v_pairs_len:] /= wei_pol[1][np.newaxis, :]
-    wei_pol /= np.nansum(wei_pol, axis = 0)[np.newaxis, :]
-
-    return wei_pairs, wei_pol
+        ## paring info
+        self.pair_info = []
+        for pol in range(num_pols_com):
+            useful_ants = self.ant_range[ch_for_reco[:, pol]]
+            if len(useful_ants) == 0:
+                pair_pol = np.full((7, 0), np.nan, dtype = float)
+                self.pair_info.append(pair_pol)
+                del useful_ants
+                continue
+            useful_time = self.hit_time_arr[useful_ants]
+            useful_pos = self.ant_pos[:, useful_ants]
+            useful_pair = np.asarray(list(combinations(useful_ants, 2))).astype(int) 
+            pair_pol = np.full((7, len(useful_pair[:, 0])), np.nan, dtype = float) # dt, x1, y1, z1, x2, y2, z2
+            pair_pol[0] = useful_time[useful_pair[:, 0]] - useful_time[useful_pair[:, 1]] 
+            pair_pol[1:4] = self.ant_pos[:, useful_pair[:, 0]] 
+            pair_pol[4:] = self.ant_pos[:, useful_pair[:, 1]]
+            self.pair_info.append(pair_pol)
+            del useful_ants, useful_time, useful_pos, useful_pair
+        del ch_for_reco
+        if use_debug == False:
+            del self.hit_time_arr           
+         
 
 
 
@@ -221,12 +158,4 @@ def get_products(weights, pairs, v_pairs_len):
 
 
 
-
-
-
-
-
-
-
-
-
+ 
