d3d5d9a820d204c3238c65f607c12c2a83f5c671
Myoungchul Kim <kmc8907@gmail.com>
2023-08-19 17:56:36 +0900

    data script

--- tools/ara_py_vertex.py
+++ tools/ara_py_vertex.py
@@ -31,7 +31,7 @@ class py_reco_handler:
 
         ## ch info
         self.ant_range = np.arange(num_ants, dtype = int)
-        self.polarizations = (self.ant_range > int(num_pols // 2 - 1)).astype(int)
+        self.polarizations = (self.ant_range > int(num_ants // 2 - 1)).astype(int)
         known_issue = known_issue_loader(st)
         self.can_use_ch = known_issue.get_bad_antenna(run, good_ant_true = True) # good chs in bool
         ara_geom = ara_geom_loader(st, int(2015))
@@ -58,13 +58,13 @@ class py_reco_handler:
         ## remove signal windo info 
         pad_no_max = np.full(self.pad_v.shape, np.nan, dtype = float)
         for ant in range(num_ants):
-            pad_no_max[:self.pad_num[:, ant], ant] = self.pad_v[:self.pad_num[:, ant], ant]
+            pad_no_max[:self.pad_num[ant], ant] = self.pad_v[:self.pad_num[ant], ant]
             front_idx = int(self.max_bin[ant] - bin_8[ant])
             if front_idx < 0:
                 front_idx = 0
             pad_no_max[front_idx:self.max_bin[ant] + bin_8[ant] + 1, ant] = np.nan
             del front_idx
-        if use_debug:
+        if self.use_debug:
             self.pad_no_max = pad_no_max
             self.bin_8 = bin_8
 
@@ -88,10 +88,10 @@ class py_reco_handler:
         self.snr_arr = (self.max_val - self.pad_mean) / self.pad_sigma        
         self.hit_time_arr = self.max_time
         nega_sigma_idx = self.pad_sigma <= 0
-        if np.count_nonzeeo(nega_sigma_idx) > 0:
+        if np.count_nonzero(nega_sigma_idx) > 0:
             self.snr_arr[nega_sigma_idx] = 0
             self.hit_time_arr[nega_sigma_idx] = -9999
-        if use_debug:
+        if self.use_debug:
             self.nega_sigma_idx = nega_sigma_idx
         del nega_sigma_idx
 
@@ -99,81 +99,112 @@ class py_reco_handler:
 
         ## input waveform
         self.pad_v = pad_v ** 2
+        self.pad_v[np.isnan(self.pad_v)] = 0
         self.pad_t = pad_t
         self.pad_num = pad_num
 
         ## hit time and snr
         self.get_ch_sliding_v2_snr_uw()
-        if use_debug == False:
+        if self.use_debug == False:
             del self.pad_v, self.pad_t, self.pad_num, self.max_bin, self.max_val, self.max_time, self.pad_mean, self.pad_sigma
 
-        ## hit check. tag only uesful ch and ch that has bigger than snr
-        is_ch_hit = np.logical_and(self.can_use_ch, self.snr_arr > self.hit_thres)
-        n_hits = np.full((num_pols_com), 0, dtype = int) # v, h, both
-        n_hits[0] = np.nansum(is_ch_hit[self.polarizations == 0])
-        n_hits[1] = np.nansum(is_ch_hit[self.polarizations == 1])
-        n_hits[2] = np.nansum(is_ch_hit)
-        if use_debug == False:
-            del self.snr_arr      
- 
         ## ch selection. tag only uesful ch, ch that has bigger than snr, same pol, and numher of hit that bigger than cut
-        ch_for_reco = np.full((num_ants, num_pols_com), 0, dtype = int)
-        ch_for_reco[:, 0] = np.all((self.polarizations == 0, is_ch_hit, n_hits[0] > num_ants_cut), axis = 0)
-        ch_for_reco[:, 1] = np.all((self.polarizations == 1, is_ch_hit, n_hits[1] > num_ants_cut), axis = 0)
-        ch_for_reco[:, 2] = np.all((is_ch_hit, n_hits[2] > num_ants_cut), axis = 0)
-        if use_debug:
-            self.is_ch_hit = is_ch_hit
-            self.n_hits = n_hits
+        ch_for_reco = np.full((num_ants, num_pols_com), False, dtype = bool)
+        ch_for_reco[:, 2] = np.logical_and(self.can_use_ch, self.snr_arr > self.hit_thres)
+        ch_for_reco[:, 0] = np.all((self.polarizations == 0, ch_for_reco[:, 2]), axis = 0)
+        ch_for_reco[:, 1] = np.all((self.polarizations == 1, ch_for_reco[:, 2]), axis = 0)
+        n_hits = np.count_nonzero(ch_for_reco, axis = 0) > self.num_ants_cut
+        ch_for_reco[:, n_hits == False] = False
+        if self.use_debug:
             self.ch_for_reco = ch_for_reco
-        del is_ch_hit, n_hits       
+            self.n_hits = n_hits
+        del n_hits
 
         ## paring info
         self.pair_info = []
+        self.useful_num_ants = np.full((num_pols_com), 0, dtype = int)
         for pol in range(num_pols_com):
             useful_ants = self.ant_range[ch_for_reco[:, pol]]
-            if len(useful_ants) == 0:
+            self.useful_num_ants[pol] = len(useful_ants)
+            if self.useful_num_ants[pol] == 0:
                 pair_pol = np.full((7, 0), np.nan, dtype = float)
                 self.pair_info.append(pair_pol)
                 del useful_ants
                 continue
-            useful_time = self.hit_time_arr[useful_ants]
-            useful_pos = self.ant_pos[:, useful_ants]
             useful_pair = np.asarray(list(combinations(useful_ants, 2))).astype(int) 
             pair_pol = np.full((7, len(useful_pair[:, 0])), np.nan, dtype = float) # dt, x1, y1, z1, x2, y2, z2
-            pair_pol[0] = useful_time[useful_pair[:, 0]] - useful_time[useful_pair[:, 1]] 
+            pair_pol[0] = self.hit_time_arr[useful_pair[:, 0]] - self.hit_time_arr[useful_pair[:, 1]] 
             pair_pol[1:4] = self.ant_pos[:, useful_pair[:, 0]] 
             pair_pol[4:] = self.ant_pos[:, useful_pair[:, 1]]
             self.pair_info.append(pair_pol)
-            del useful_ants, useful_time, useful_pos, useful_pair
+            del useful_ants, useful_pair
         del ch_for_reco
-        if use_debug == False:
-            del self.hit_time_arr           
          
 class py_ara_vertex:
 
-    def __init__(self):
+    def __init__(self, st):
 
         ara_geom = ara_geom_loader(st, int(2015))
         self.ant_pos_mean = ara_geom.get_ant_xyz(use_mean = True) # (xyz)
         del ara_geom
 
         self.reco = ROOT.AraVertex()
-        self.reco.SetLOG(self.ant_pos_mean[0], self.ant_pos_mean[1], self.ant_pos_mean[2])
+        self.reco.SetCOG(self.ant_pos_mean[0], self.ant_pos_mean[1], self.ant_pos_mean[2])
 
-    def get_add_pair(self, pair_num):
-
-        pair_indi = self.pair_info[pair_num]
-        pair_len = len(pair_indi)
+    def get_add_pair(self, pair_indi, pair_len):
 
         self.reco.clear()
         for pair in range(pair_len):
-            self.reco.addPair(pair_indi[0], pair_indi[1], pair_indi[2], pair_indi[3], pair_indi[4], pair_indi[5], pair_indi[6])
+            self.reco.addPair(pair_indi[0, pair], pair_indi[1, pair], pair_indi[2, pair], pair_indi[3, pair], pair_indi[4, pair], pair_indi[5, pair], pair_indi[6, pair])
 
     def get_print_pair(self):
 
         self.reco.printPairs()
 
+    def get_pair_fit_spherical(self, pair_info, useful_num_ants):
+
+        self.theta = np.full((num_pols_com), np.nan, dtype = float) 
+        self.phi = np.copy(self.theta)
+        for pol in range(num_pols_com):
+            if useful_num_ants[pol] == 0:
+                continue
+            if pol == 2:
+                pass_flag = True
+                if useful_num_ants[pol] == useful_num_ants[0]:
+                    pol_copy = 0
+                elif useful_num_ants[pol] == useful_num_ants[1]:
+                    pol_copy = 1
+                else:
+                    pass_flag = False
+                if pass_flag:    
+                    self.theta[pol] = self.theta[pol_copy]
+                    self.phi[pol] = self.phi[pol_copy]
+                    del pol_copy
+                    continue
+                else:
+                    pass
+
+            pair_indi = pair_info[pol]
+            pair_len = len(pair_indi)
+            
+            self.get_add_pair(pair_indi, pair_len)
+            #self.get_print_pair()
+
+            fit_result = self.reco.doPairFitSpherical()
+            self.theta[pol] = fit_result.theta
+            self.phi[pol] = fit_result.phi
+            del pair_indi, pair_len, fit_result
+
+        self.theta = 90 - np.degrees(self.theta)
+        self.phi = np.degrees(self.phi)
 
 
 
- 
+
+
+
+
+
+
+
+
