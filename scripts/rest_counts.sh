00590e3a705d168cdd41e6c601664b12d493033a
Myoungchul Kim <kmc8907@gmail.com>
2023-12-19 16:39:13 +0900

    asfadsas

---
+++ scripts/rest_counts.sh
@@ -0,0 +1,2 @@
+python3 file_raw_counts.py 2 1 /misc/disk19/users/mkim/OMF_filter/ARA02/temp/ /home/mkim/analysis/MF_filters/data/raw_list/
+python3 file_raw_counts.py 3 1 /misc/disk19/users/mkim/OMF_filter/ARA03/temp/ /home/mkim/analysis/MF_filters/data/raw_list/
