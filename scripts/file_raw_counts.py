1d0c18b471a94295b15a5c7e878addc075c843be
Myoungchul Kim <kmc8907@gmail.com>
2023-12-21 21:45:47 +0900

    asdasd

--- scripts/file_raw_counts.py
+++ scripts/file_raw_counts.py
@@ -22,6 +22,20 @@ if Blind == 1:
 else:
     blind_type = ''
 
+mvd_name = f'../data/raw_list/A{Station}_raw_list{blind_type}_b.txt'
+mvd_file =  open(mvd_name, "r")
+mvd_num = []
+mvd_log = []
+for lines in mvd_file:
+    line = lines.split()
+    mvd_nums = int(line[0])
+    mvd_num.append(mvd_nums)
+    mvd_log.append(line[1])
+    del line
+mvd_file.close()
+mvd_num = np.asarray(mvd_num, dtype = int)
+print('Moved #:', len(mvd_num))
+
 raw_log = glob(f'{Input}event*')
 raw_len = len(raw_log)
 raw_name = f'{Output}A{Station}_raw_list{blind_type}.txt'
@@ -29,6 +43,10 @@ with open(raw_name, 'w') as f:
     for l in tqdm(range(raw_len)):
         raw_run = int(get_path_info_v2(raw_log[l], f'/event', '.root'))        
         raw_txt = f'{raw_run} {raw_log[l]} \n'
+        f.write(raw_txt)
+
+    for l in tqdm(range(len(mvd_num))):
+        raw_txt = f'{mvd_num[l]} {mvd_log[l]} \n'
         f.write(raw_txt)
 
 print(raw_name, size_checker(raw_name))
